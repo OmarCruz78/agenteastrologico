bebc8c7b1b7c6bee69a8beae2bfa2ea86a78b391
Omar Cruz <harekrshnayoga@gmail.com>
2025-09-26 08:33:04 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,3 @@
-const express = require("express");
-
-const app = express();
-const port = 3000; // puedes cambiarlo si ya usas ese puerto
-
-// Endpoint de prueba
-app.get("/ping", (req, res) => {
-  res.json({ message: "pong 🏓", status: "ok" });
-});
-
-//app get
-app.get("/", (req, res) => {
-  res.send("🚀 Servidor funcionando en Render");
-});
-
-app.get("/api/status", (req, res) => {
-  res.json({ status: "ok", time: new Date() });
-});
-
-
-
-// Iniciar servidor
-app.listen(port, () => {
-  console.log(`✅ Servidor corriendo en http://localhost:${port}`);
-});
-
-
-app.get("/api/horoscope", (req, res) => {
-  res.json({
-    sign: "Aries",
-    prediction: "Hoy es un buen día para empezar algo nuevo 🔮",
-    date: new Date().toLocaleDateString()
-    });
-});
-
 
 import express from "express";
 import path from "path";
